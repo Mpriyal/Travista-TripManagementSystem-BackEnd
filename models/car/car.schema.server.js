eaaa983f9719adc08e1f97a9fdc5d314c89ccaa1
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-06-23 03:52:53 -0400

    Added schema for car domain object

--- models/car/car.schema.server.js
+++ models/car/car.schema.server.js
@@ -1,10 +1,34 @@
 var mongoose = require('mongoose');
 
 var carSchema = mongoose.Schema({
-    name: String,
-    model: String,
-    age: Number,
-    price: String
+    provider: {
+        company_name: String,
+    },
+    location: {
+        latitude: Number,
+        longitude: Number
+    },
+    address: {
+        line1: String,
+        city: String,
+        region: String,
+        country: String
+    },
+    cars: [{
+        vehicle_info: {
+            transmission: String,
+            fuel: String,
+            air_conditioning: String,
+            category: String,
+            type: String
+        },
+    rates: {
+        price : {
+            type: String,
+            amount: String,
+            currency: String
+        }
+    }}]
 }, {collection: 'car'});
 
 module.exports = carSchema;
