37b67d4000d25987e7da4e48b0db860510ba8925
Ayush Shukla <ashukla2705@gmail.com>
2018-06-26 03:49:43 -0400

    Heroku links added and Mongo Db configured

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express')
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const idleTimeoutSeconds = 30*60*1000;
-// mongoose.connect('mongodb://heroku_25rt1ndf:oq7m9k01j53va5sg677eo9qpib@ds117061.mlab.com:17061/heroku_25rt1ndf');
-mongoose.connect('mongodb://localhost/travel-manager');
+mongoose.connect('mongodb://heroku_25rt1ndf:oq7m9k01j53va5sg677eo9qpib@ds117061.mlab.com:17061/heroku_25rt1ndf');
+// mongoose.connect('mongodb://localhost/travel-manager');
 
 var app = express()
 
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin",
-    "http://localhost:3000");
+    "http://webdev-travista-client.herokuapp.com");
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
